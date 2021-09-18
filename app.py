2fc5ca1880bd2e7b161fefeec27e83431b87a05c
unknown <russo555@gmail.com>
2021-09-18 10:55:24 -0400

    Added bootstrap CSS.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import argparse
 import os
 import csv
 import sys
+from pathlib import Path
 
 from flask import Flask, redirect
 
@@ -112,6 +113,7 @@ def hello_world():
     output = ("""
     <html>
     <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <style>
     td, th {
     border: 1px solid #ddd;
@@ -144,6 +146,8 @@ def hello_world():
     </script>
     </head>
     <body>
+    <h1>Playlist: """ + Path(os.path.basename(tsv_file.name)).stem + """</h1>
+    <p>Click on any clue to start playback. Clue links are disabled until playback is finished.</p>
     <table>
     """ + generate_html() +
     """
